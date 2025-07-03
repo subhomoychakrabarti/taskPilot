bdcb11ce859988d4571445b646558f56c114bf3f
subhomoychakrabarti <subhomoy.chakrabarti@gmail.com>
2025-07-03 23:03:23 +0530

    removedtask Service

--- TaskService/src/main/java/com/taskpilot/task/exception/ValidationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.taskpilot.exception;
-
-public class ValidationFailedException extends RuntimeException {
-    public ValidationFailedException(String message) {
-        super(message);
-    }
-}
--- TaskService/src/main/java/com/taskpilot/task/feign/RegistrationClient.java
+++
@@ -1,8 +0,0 @@
-package com.taskpilot.feign;
-
-
-@FeignClient(name = "registrationClient", url = "${taskpilot.registration.url}")
-public interface RegistrationClient {
-    @PatchMapping("/registrationService/handler/updateTask")
-    ResponseEntity<void> updateTask(@ UpdateTaskRequest request);
-}
